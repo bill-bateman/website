86d5cf81910a330779e25a51bc57166ede74357d
Bill Bateman <bill@batemanzhou.com>
2021-02-20 17:57:20 -0800

    Adding more info to postlist. Also fixing subcategory filtering.

--- src/components/filter-posts.js
+++ src/components/filter-posts.js
@@ -8,6 +8,9 @@ function FilterPosts(props) {
 	function postLinkToCategory(elem) {
 		return elem.getAttribute('data-category');
 	}
+	function postLinkToSubcategory(elem) {
+		return elem.getAttribute('data-subcategory');
+	}
 
 	function filterTitleText(elem, title_filter) {
 		return postLinkToTitle(elem).toLowerCase().indexOf(title_filter) > -1;
@@ -15,13 +18,17 @@ function FilterPosts(props) {
 	function filterCategory(elem, category_filter) {
 		return category_filter==='all' || category_filter===postLinkToCategory(elem)
 	}
+	function filterSubcategory(elem, subcategory_filter) {
+		return subcategory_filter==='-select category-' || subcategory_filter==='all' || subcategory_filter===postLinkToSubcategory(elem)
+	}
 
 	function filter() {
 		var title_filter = document.getElementById('titleFilterText').value.toLowerCase();
 		var category_filter = document.getElementById('categoryFilterSelect').value.toLowerCase();
+		var subcategory_filter = document.getElementById('subcategoryFilterSelect').value.toLowerCase();
 		var elems = document.getElementsByName('postlink');
 		for (var i=0; i<elems.length; i++) {
-			if (filterTitleText(elems[i], title_filter) && filterCategory(elems[i], category_filter)) {
+			if (filterTitleText(elems[i], title_filter) && filterCategory(elems[i], category_filter) && filterSubcategory(elems[i], subcategory_filter)) {
 				elems[i].style.display = "";
 			} else {
 				elems[i].style.display = "none";
@@ -69,7 +76,7 @@ function FilterPosts(props) {
 				{CategoryOptions}
 			</select>
 			<select id="subcategoryFilterSelect" onChange={filter}>
-				<option key="-select category-" value="-select category-">-select category</option> 
+				<option key="-select category-" value="-select category-">-select category-</option> 
 			</select>
 		</div>
 		{props.children}
--- src/components/post-link.js
+++ src/components/post-link.js
@@ -3,15 +3,18 @@ import { Link } from "gatsby"
 import Img from "gatsby-image"
 
 const PostLink = ({ post }) => (
-  <div name="postlink" data-category={post.frontmatter.slug.split('/')[1]}>
+  <div name="postlink" data-category={post.frontmatter.slug.split('/')[1]} data-subcategory={post.frontmatter.slug.split('/')[2]}>
   	<hr/>
     <Link to={post.frontmatter.slug} className="listlink">
     	<Img className="listfeature" fluid={post.frontmatter.featuredImage.childImageSharp.fluid} />
+      <p className="listinfo">
+        {post.frontmatter.slug.split('/')[1]} / {post.frontmatter.slug.split('/')[2]}
+        <span>
+          {post.frontmatter.date}
+        </span>
+      </p>
     	<p className="listtext">
     		{post.frontmatter.title}
-    		<span>
-    			{post.frontmatter.date}
-    		</span>
     	</p>
     </Link>
   </div>
