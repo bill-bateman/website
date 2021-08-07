55b9370109c936e4f443b271a2ff3657e8714c2d
bill-bateman <bill@batemanzhou.com>
2021-08-07 12:15:45 -0700

    Trouble with ssh

--- .github/deploy.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p "$HOME/.ssh"
-echo "$DEPLOY_KEY" >"$HOME/.ssh/key"
-chmod 600 "$HOME/.ssh/key"
-rsync -avz --delete -e 'ssh -i $HOME/.ssh/key' ${LOCAL_BUILD_DIRECTORY} ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_DIRECTORY_NAME}
-rm "$HOME/.ssh/key"
