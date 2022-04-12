b98262ce2856f990a8e7a9611ed807686f8496b9
bill-bateman <bill@batemanzhou.com>
2022-04-11 17:29:53 -0700

    Slimming down content. Removing resume/about pages.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
 import React from "react"
 import Layout from "../components/layout"
+
 import olomana from "../content/images/olomana.png"
+import linkedin from "../content/images/linkedin.png"
+import mail from "../content/images/mail.png"
 
 export default function Home() {
   return (
     <Layout title="Bill Bateman" subtitle="Personal Website">
       <p>
-        Welcome to my website.
+      My name is Bill. I am a software developer working remotely in British Columbia.
         <br /><br />
-        I use this site for a variety of things, including: resume, blog posts, solo transcriptions, explorations of different computer science topics, and notes.
+        I use this site for blog posts and personal projects.
+        <ul className="nobullets">
+          <li><b>Contact:</b></li>
+          <li><img alt="Mail Icon" width="15px" src={mail} />  bill at batemanzhou dot com</li>
+          <li><img alt="LinkedIn Icon" width="15px" src={linkedin} />  <a href="https://www.linkedin.com/in/wsbateman/">Bill Bateman | LinkedIn</a></li>
+        </ul>
       </p>
       <img width="100%" alt="Olomana Trail" src={olomana} />
       <p className="imagecaption">Third peak of the Olomana Trail.</p>
